d943e2b48cf22b394bd6b8a5668135e7f8644101
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2021-11-23 16:11:25 +0100

    LPS-142929 remove abstract to fix compilation error

--- portal-kernel/src/com/liferay/portal/kernel/upgrade/MVCCVersionUpgradeProcess.java
+++ portal-kernel/src/com/liferay/portal/kernel/upgrade/MVCCVersionUpgradeProcess.java
@@ -27,7 +27,7 @@ import java.sql.ResultSet;
 /**
  * @author Shuyang Zhou
  */
-public abstract class MVCCVersionUpgradeProcess extends UpgradeProcess {
+public class MVCCVersionUpgradeProcess extends UpgradeProcess {
 
 	public void upgradeMVCCVersion(
 			DatabaseMetaData databaseMetaData, String tableName)
