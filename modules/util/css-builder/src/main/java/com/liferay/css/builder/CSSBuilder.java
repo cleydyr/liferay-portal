2cdff24568f53d06d40f34018b1227508bbdae1d
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2021-12-07 09:55:25 +0100

    dart sass test

--- modules/util/css-builder/src/main/java/com/liferay/css/builder/CSSBuilder.java
+++ modules/util/css-builder/src/main/java/com/liferay/css/builder/CSSBuilder.java
@@ -22,6 +22,7 @@ import com.liferay.css.builder.internal.util.FileUtil;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.rtl.css.RTLCSSConverter;
 import com.liferay.sass.compiler.SassCompiler;
+import com.liferay.sass.compiler.dart.internal.DartSassCompiler;
 import com.liferay.sass.compiler.jni.internal.JniSassCompiler;
 import com.liferay.sass.compiler.jsass.internal.JSassCompiler;
 import com.liferay.sass.compiler.ruby.internal.RubySassCompiler;
@@ -330,6 +331,19 @@ public class CSSBuilder implements AutoCloseable {
 			catch (Exception exception) {
 				System.out.println(
 					"Unable to load Ruby compiler, falling back to native");
+
+				_sassCompiler = new JSassCompiler(precision);
+			}
+		}
+		else if (sassCompilerClassName.equals("dart")) {
+			try {
+				_sassCompiler = new DartSassCompiler();
+
+				System.out.println("Using Dart Sass compiler");
+			}
+			catch (Exception exception) {
+				System.out.println(
+					"Unable to load Dart compiler, falling back to native");
 
 				_sassCompiler = new JSassCompiler(precision);
 			}
